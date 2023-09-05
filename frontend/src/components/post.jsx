e300e9856947efd1ac86209b1185c7b09aacfaec
Gerry Wright <gerry.c.wright@googlemail.com>
2023-09-05 19:22:27 +0100

    guard

--- frontend/src/components/post.jsx
+++ frontend/src/components/post.jsx
@@ -1,27 +1,31 @@
-import { useContext} from 'react';
+import { useContext } from 'react';
 import { store } from '../store.js';
 import profilePicture from "../resources/grey-profile-picture.png";
-// const postBody = useContext(store).state.posts.post
 
 
-export default function Post({post}) {
+export default function Post({ post }) {
     const userDetails = useContext(store).state.userDetails;
     console.log(post)
 
     //TODO create a func that gets extension post.imageUrl, if returns emtpy return null.
 
-
+    //TODO conditonal render post.imageUrl based on its extension/if it exists
 
     return (
         <div className="home-post-container">
             <div className="user-info">
-                <img src={profilePicture} alt="profile-picture" className="profile-picture"/>
+                <img src={profilePicture} alt="profile-picture" className="profile-picture" />
                 <div className="profile-name">{userDetails.email}</div>
             </div>
             <div className="post-display">
-                <p>{post.post}</p>
-                //TODO conditonal render post.imageUrl based on its extension/if it exists
-                <img src={post.imageUrl} />
+                <p className="post-body">{post.post}</p>
+                <div className="media-display">
+                    {<img src={post.imageUrl} />}
+                    {<video>
+                        <source src="movie.mp4" type="video/mp4" />
+                        <source src="movie.ogg" type="video/ogg" />
+                    </video>}
+                </div>
             </div>
         </div>
     )
